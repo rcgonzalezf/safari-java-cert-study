3c7c01f292118adb2222f0b3a0cfadefff597bed
Simon Roberts <simon@dancingcloudservices.com>
2020-04-27 09:30:31 -0600

    diagram of static

---
+++ src/main/java/usingstatic/Q4.java
@@ -0,0 +1,20 @@
+package usingstatic;
+
+class X {
+  static int MAX_VAL = 100;
+  int value = 99;
+  public void setVal(int x) {
+    if (x <= MAX_VAL) value = x;
+  }
+}
+public class Q4 {
+  public static void main(String[] args) {
+    X a = new X();
+    X b = new X();
+    a.setVal(120);
+    System.out.print(a.value + ", ");
+    b.MAX_VAL = 150;
+    a.setVal(140);
+    System.out.println(a.value);
+  }
+}
