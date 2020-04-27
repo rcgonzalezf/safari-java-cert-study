be2e2f56bb30ad91a2110ae727fa608bedc2cdbe
Simon Roberts <simon@dancingcloudservices.com>
2020-04-27 11:04:36 -0600

    Day 1 final

--- src/main/java/usingstatic/Q5.java
+++ src/main/java/usingstatic/Q5.java
@@ -1,5 +1,11 @@
 package usingstatic;
 
+interface P {
+  static String sayHi() {
+    return "P-hello";
+  }
+}
+
 class Y {
   static String sayHi() { return "Y-Hello"; }
   String sayHi2() { return "Y-Hello2"; }
@@ -14,5 +20,9 @@ public class Q5 {
   public static void main(String[] args) {
     Y y = new Z();
     System.out.println(y.sayHi() + ", " + y.sayHi2());
+
+    P p = null;
+    P.sayHi(); // OK
+//    p.sayHi(); // NOT OK -- can't do this with interface static methods
   }
 }
